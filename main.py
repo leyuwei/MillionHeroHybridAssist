03a595532d0c749886bfe141e4376e83e99917fd
LeYuwei <leyuwei126@126.com>
2018-02-02 13:24:39 +0800

    Add files via upload
    升级了双线程模式

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import multiprocessing
 from multiprocessing import Event
 from multiprocessing import Pipe
 
+import threading
+from threading import Thread
 import time
 from argparse import ArgumentParser
 from multiprocessing import Value
@@ -39,6 +41,11 @@ from core.chrome_search import run_browser
 from core.ocr.baiduocr import get_text_from_image as bai_get_text
 from core.ocr.spaceocr import get_text_from_image as ocrspace_get_text
 
+global isNegativeQuestion
+global origQuestion
+isNegativeQuestion = False
+origQuestion = ""
+
 if prefer[0] == "baidu":
     get_text_from_image = partial(bai_get_text,
                                   app_id=app_id,
@@ -46,10 +53,8 @@ if prefer[0] == "baidu":
                                   app_secret=app_secret,
                                   api_version=api_version,
                                   timeout=5)
-
 elif prefer[0] == "ocrspace":
     get_test_from_image = partial(ocrspace_get_text, api_key=api_key)
-
 
 def parse_args():
     parser = ArgumentParser(description="Million Hero Assistant")
@@ -63,6 +68,7 @@ def parse_args():
 
 
 def parse_question_and_answer(text_list):
+    global origQuestion
     question = ""
     start = 0
     for i, keyword in enumerate(text_list):
@@ -74,10 +80,22 @@ def parse_question_and_answer(text_list):
             start = i + 1
             break
     real_question = question.split(".")[-1]
+    origQuestion = real_question
+    # 新增题目模式识别
+    global isNegativeQuestion
+    isNegativeQuestion = False
+    if real_question.find('没有')>=0 or real_question.find('未')>=0 or real_question.find('不是')>=0 or real_question.find('是错')>=0:
+        isNegativeQuestion = True
+        real_question = real_question.replace('没有','有')
+        real_question = real_question.replace('未', '')
+        real_question = real_question.replace('还未', '已')
+        real_question = real_question.replace('不是', '是')
+        real_question = real_question.replace('是错', '是对')
     question, true_flag = parse_false(real_question)
     # 增加识别异常符号处理
     for ii in range(start,len(text_list)):
         text_list[ii] = re.sub("[\s+\.\!\/_,《》√✔×✘↘→↗↑↖←↙↓\“\”·$%^*(+\’\‘\']+|[+——！，。？、~@#￥%……&*（）]+", "", text_list[ii])
+        text_list[ii] = text_list[ii].lower()
     return true_flag, real_question, question, text_list[start:]
 
 
@@ -89,12 +107,26 @@ def pre_process_question(keyword):
     """
     for char, repl in [("“", ""), ("”", ""), ("？", "")]:
         keyword = keyword.replace(char, repl)
-
     keyword = keyword.split(r"．")[-1]
     keywords = keyword.split(" ")
     keyword = "".join([e.strip("\r\n") for e in keywords if e])
     return keyword
 
+
+class SearchThread(Thread):
+    def __init__(self, question,answer,timeout,engine):
+        Thread.__init__(self)
+        self.question = question
+        self.answer = answer
+        self.timeout = timeout
+        self.engine = engine
+    def run(self):
+        if self.engine == 'baidu':
+            self.result = baidu_count(self.question,self.answer,timeout=self.timeout)
+        else:
+            self.result = bing_count(self.question,self.answer,timeout=self.timeout)
+    def get_result(self):
+        return self.result
 
 def main():
     args = parse_args()
@@ -112,6 +144,7 @@ def main():
         browser_daemon.start()
 
     def __inner_job():
+        global isNegativeQuestion,origQuestion
         start = time.time()
         text_binary = analyze_current_screen_text(
             directory=data_directory,
@@ -122,13 +155,13 @@ def main():
             image_data=text_binary,
         )
         if not keywords:
-            print("text not recognize")
+            print("本题不能够识别！")
             return
 
         true_flag, real_question, question, answers = parse_question_and_answer(keywords)
-        print('-' * 72)
-        print(real_question)
-        print('-' * 72)
+        print('-' * 60)
+        print(origQuestion)
+        print('-' * 60)
         print("\n".join(answers))
 
         # notice browser
@@ -137,10 +170,21 @@ def main():
             noticer.set()
 
         search_question = pre_process_question(question)
-        summary = baidu_count(search_question, answers, timeout=timeout)
-        # 获取百度结果
-        summary2 = bing_count(search_question, answers, timeout=timeout)
-        # 获取bing词典结果
+        thd1 = SearchThread(search_question, answers, timeout, 'baidu')
+        thd2 = SearchThread(search_question, answers, timeout, 'bing')
+        # 创立双并发线程
+        if __name__ == '__main__':
+            thd1.setDaemon(True)
+            thd1.start()
+            thd2.setDaemon(True)
+            thd2.start()
+            # 顺序开启两线程
+            thd1.join()
+            thd2.join()
+            # 等待两线程执行结束
+        summary = thd1.get_result()
+        summary2 = thd2.get_result()
+        # 获取线程执行结果
 
         # 下面开始合并结果并添加可靠性标志
         creditFlag = True
@@ -152,24 +196,35 @@ def main():
         if credit < 2:
             creditFlag = False
 
-        summary_li = sorted(
-            summary_t.items(), key=operator.itemgetter(1), reverse=True)
+        if isNegativeQuestion == False:
+            summary_li = sorted(summary_t.items(), key=operator.itemgetter(1), reverse=True)
+        else:
+            summary_li = sorted(summary_t.items(), key=operator.itemgetter(1), reverse=False)
         data = [("选项", "权重")]
+        topscore = 0
         for a, w in summary_li:
-            data.append((a, w))
+            if w > topscore:
+                topscore = w
+        for a, w in summary_li:
+            if isNegativeQuestion==False:
+                data.append((a, w))
+            else:
+                data.append((a, topscore-w))
         table = AsciiTable(data)
         print(table.table)
 
-        print("*" * 72)
+        print("*" * 60)
         if creditFlag == False:
-            print("！！！ 本题预测结果不是很可靠，请三思后行 ！！！")
+            print("        ！！！ 本题预测结果不是很可靠，请三思后行 ！！！\n        ！！！ 本题预测结果不是很可靠，请三思后行 ！！！")
+        if isNegativeQuestion==True:
+            print("        --- 本题是否定提法，程序已帮您优化结果！ ---")
         if true_flag:
-            print("肯定回答(***)： ", summary_li[0][0])
-            print("否定回答(   )： ", summary_li[-1][0])
+            print("+ 建议选项 ： ", summary_li[0][0])
+            print("- 排除选项 ： ", summary_li[-1][0])
         else:
-            print("肯定回答(   )： ", summary_li[0][0])
-            print("否定回答(***)： ", summary_li[-1][0])
-        print("*" * 72)
+            print("+ 建议选项 ： ", summary_li[0][0])
+            print("- 排除选项 ： ", summary_li[-1][0])
+        print("*" * 60)
 
         end = time.time()
         print("耗时 {0} 秒".format(end - start))
@@ -178,12 +233,12 @@ def main():
         )
 
     print("""
-    原作者：GitHub/smileboywtu
+    原作者：GitHub/lingfengsan
     Branch作者：GitHub/leyuwei
-    Branch改进：新增百度必应混合搜索，减小超时检测时间
-                改进算法复杂度，使用正则处理降低文本分析量
-                改进识别乱码时的处理能力
-                新增非可靠结果的明显提示
+    Branch改进：优化算法的匹配模式
+                新增否定题搜索识别模式
+                新增双线程并发搜索
+                修正BUG们
     请选择答题节目: 
       1. 百万英雄
       2. 冲顶大会
